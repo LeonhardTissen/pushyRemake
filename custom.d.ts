0948263d2397cf428fe6acaf4cfb8e72c97dda2f
Leonhard Tissen <contact@warze.org>
2023-11-17 18:00:12 +0100

    Revert "Added project files"
    This reverts commit 00f61bcd64a8d2e68b4766b0ee96a89772c0165e.

--- custom.d.ts
+++
@@ -1,8 +0,0 @@
-declare module '*.png' {
-    const value: string;
-    export default value;
-}
-declare module '*.frag' {
-    const value: string;
-    export default value;
-}
--- sprites.sh
+++
@@ -1,5 +0,0 @@
-npx spritesheet-js assets/game/*.png -f pixi.js -p src/assets/imgs/ -n game.spritesheet #--trim
-optipng src/assets/**/*.png
-# correct the ref
-jq --tab '.meta.image = "@ref(./\(.meta.image))"' src/assets/imgs/game.spritesheet.json > src/assets/imgs/game.spritesheet.json.tmp
-mv src/assets/imgs/game.spritesheet.json.tmp src/assets/imgs/game.spritesheet.json
